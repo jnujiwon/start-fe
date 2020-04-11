82e7ce6ef9250ba672495cd592c3bdb2d896375a
jnujiwon <55755351+jnujiwon@users.noreply.github.com>
2020-04-12 04:05:46 +0900

    03

---
+++ 03/app.js
@@ -0,0 +1,44 @@
+console.log(window.outerWidth);
+console.log(window.name);
+// window.open('http://www.daum.net');
+
+//var debug = console.log(document.getElementById('debug'));
+// console.log(debug);
+
+// var box = document.querySelector('#debug ');
+// console.log(box)
+
+// var box = document.querySelector('.box');
+// var box2 = document.querySelectorAll('.box'); // 배열은 아니지만 배열처럼 보임
+// console.log(box2); 
+
+// var div = document.createElement('div');
+// // console.log(div);
+// div.style.border = '1px solid #000';
+// div.innerHTML = 'hello!';
+
+
+// document.body.appendChild(div);
+
+// const span = document.createElement('span');
+// const textNode = document.createTextNode('hello!'); 
+// span.appendChild(textNode);
+// document.querySelector('#debug').appendChild(span);
+
+// var debug = document.getElementById('debug');
+// // debug.remove();
+
+// const list = document.getElementById('list'); 
+// list.removeChild(list.children[0]);
+
+// var clickCount = 0;
+// function popup(event){
+//     console.log(++clickCount);
+//     console.log(arguments, this ,event ,event.type,event.currentTarget);
+// }
+// document.body.addEventListener('click', popup);
+
+// document.body.addEventListener('click', function(){
+//     console.log("click");
+// });
+
